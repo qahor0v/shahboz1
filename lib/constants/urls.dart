a57d0c248fe7f522ba99eaf534142171c60e74c6
qahor0v <fluttuzgroup@gmail.com>
2022-11-02 04:42:27 +0500

    4:42 / 02.11.2022

---
+++ lib/constants/urls.dart
@@ -0,0 +1,4 @@
+const String instagram = "https://www.instagram.com/shahboz_sattorov1/";
+const String telegram = "https://t.me/Shahboz_Sattorov";
+const String youtube = "https://www.youtube.com/c/ShahbozSattorov";
+const String tiktok = "https://www.tiktok.com/@shahboz_sattorov";
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:shahboz1/screens/sidebar.dart';
+import 'package:shahboz1/constants/urls.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = "home";
@@ -12,16 +13,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      drawer: Sidebar(),
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-        flexibleSpace: Container(),
-      ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
+          children: const [
             Home1(),
           ],
         ),
@@ -49,7 +43,7 @@ class _Home1State extends State<Home1> {
       sizedBx = height * 0.6;
     } else {
       alignment = Alignment.topRight;
-      sizedBx = height*0.1;
+      sizedBx = height * 0.1;
     }
     return SizedBox(
       height: MediaQuery.of(context).size.height * 2,
@@ -71,9 +65,9 @@ class _Home1State extends State<Home1> {
           ),
           Padding(
             padding: EdgeInsets.only(
-              left: width * 0.1,
-              right: width * 0.1,
-              top: height * 0.1,
+              left: width * 0.2,
+              right: width * 0.2,
+              top: height * 0.2,
             ),
             child: Image.asset(
               "assets/images/shahboz2.png",
@@ -103,13 +97,14 @@ class _Home1State extends State<Home1> {
             ),
           ),
           Column(
-            //mainAxisAlignment: MainAxisAlignment.end,
+            //mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: sizedBx,
               ),
-              textWidget(width)
+              textWidget(width),
+              forDesktop(height, width)
             ],
           )
         ],
@@ -117,51 +112,146 @@ class _Home1State extends State<Home1> {
     );
   }
 
-  textWidget(double width) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 5,
-          sigmaY: 5,
+
+  Widget forDesktop(height, width) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: height * 0.1,
+        bottom: height * 0.1,
+        left: width * 0.01,
+        right: width * 0.01,
+      ),
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            socialScreen(548000, FontAwesomeIcons.youtube, youtube, width),
+            socialScreen(160000, FontAwesomeIcons.instagram, instagram, width),
+            socialScreen(2280, FontAwesomeIcons.telegram, telegram, width),
+            socialScreen(242000, FontAwesomeIcons.tiktok, tiktok, width),
+            // socialScreen(170000, FontAwesomeIcons.instagram, "", width),
+          ],
         ),
-        child: Container(
-          // height: MediaQuery.of(context).size.height * 0.075,
-          margin: const EdgeInsets.only(
-            left: 12,
-            right: 12,
-          ),
-          padding:
-              const EdgeInsets.only(left: 12, right: 12, top: 20, bottom: 20),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.0),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Shahboz\nSattorov',
-                style: GoogleFonts.zcoolKuaiLe(
-                    fontSize: width*0.1,
-                    fontWeight: FontWeight.w700,
-                    //fontStyle: FontStyle.italic,
-                    color: const Color(0xffA1D6E2)),
+      ),
+    );
+  }
+
+  Widget socialScreen(int followers, icon, String url, width) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 12, right: 12, bottom: 20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: MaterialButton(
+          onPressed: () {
+            _launchUrl(url);
+          },
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 5,
+              sigmaY: 5,
+            ),
+            child: Container(
+              width: width * 0.15,
+              // height: MediaQuery.of(context).size.height * 0.075,
+              padding: EdgeInsets.only(
+                left: width * 0.01,
+                right: width * 0.01,
+                top: width * 0.01,
+                bottom: width * 0.01,
               ),
-              Text(
-                'Bolger | Aktyor | Qiziqchi | Muallif',
-                style: GoogleFonts.actor(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    //fontStyle: FontStyle.italic,
-                    color: const Color(0xffF1F1F2)
+              decoration: BoxDecoration(
+                color: const Color(0xffBCBABE).withOpacity(0.3),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FaIcon(
+                      icon,
+                      size: width * 0.1,
+                      color: Color(0xffBCBABE),
+                    ),
+                    Text(
+                      "${followers ~/ 1000}K",
+                      style: GoogleFonts.forum(
+                          fontSize: width * 0.04,
+                          fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          color: const Color(0xffF1F1F2)),
+                    ),
+                    Text(
+                      "OBUNACHI",
+                      style: GoogleFonts.odorMeanChey(
+                          fontSize: width * 0.02,
+                          fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          color: const Color(0xffBCBABE)),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  textWidget(double width) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12,
+        right: 12,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 5,
+            sigmaY: 5,
+          ),
+          child: Container(
+            // height: MediaQuery.of(context).size.height * 0.075,
+            padding:
+                const EdgeInsets.only(left: 12, right: 12, top: 20, bottom: 20),
+            decoration: BoxDecoration(
+              color: const Color(0xffBCBABE).withOpacity(0.3),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Shahboz\nSattorov',
+                  style: GoogleFonts.zcoolKuaiLe(
+                      fontSize: width * 0.1,
+                      fontWeight: FontWeight.w700,
+                      //fontStyle: FontStyle.italic,
+                      color: const Color(0xffA1D6E2)),
+                ),
+                Text(
+                  'Bolger | Aktyor | Stand Up Comic | Muallif',
+                  style: GoogleFonts.actor(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      //fontStyle: FontStyle.italic,
+                      color: const Color(0xffF1F1F2)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _launchUrl(String url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw 'Could not launch $url';
+    }
   }
 }
